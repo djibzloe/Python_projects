60cda10c8d441c79470e6978cc43ace6ade7e4d7
Djibzloe <djibzloe@gmail.com>
2019-11-19 11:26:46 +0300

    commit book 1

---
+++ Dev01.py
@@ -0,0 +1,44 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import csv
+
+
+url = "https://www.777555.by/kompyutery_i_seti/kompyutery_i_komplektuyuschie/ssd/"
+
+
+def get_html(url):
+    response = requests.get(url)
+    return response.text
+
+def write_csv(data):
+    with open("data.csv", "a") as file:
+        writer = csv.writer(file)
+
+        writer.writerow((data["title"],data["price"],data["link"]))
+
+def get_links(html):
+    soup = BeautifulSoup(html,"lxml")
+    catalog = soup.find_all("div", class_="catalog-item")
+    print(type(catalog))
+    #print(catalog)
+
+
+    for item in catalog:
+        link = item.find("div", class_="catalog-item-title").find("a").get("href")
+        title = item.find("div", class_="catalog-item-title").find("a").text
+        price = item.find("div", class_="catalog-item-price").text
+        # print("title = " + str(title))
+        # print("price = " + str(price))
+        # print("link = " + str(link))
+        data = {"link": link, "title": title, "price":price}
+
+
+        write_csv(data)
+
+
+html = get_html(url)
+get_links(html)
+print("DONE")
+
+
