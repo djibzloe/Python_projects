62221b8c9f01bdd962b4efcc8b1e0b882c8bcfa5
Djibzloe <djibzloe@gmail.com>
2019-11-22 21:46:19 +0300

    com 4

--- Dev01.py
+++ Dev01.py
@@ -3,47 +3,43 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import csv
 
-# 123
-a=1
+# csv:  price , BYN , / , units , currency abbreviation (exmpl:  2,0479 BYN / 1 USD)
 
-def pr11():
-    print('123')
+url = "https://myfin.by/bank/kursy_valjut_nbrb"
+headers = {"accept": "*/*", "user-agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"}
 
-url = "https://www.777555.by/kompyutery_i_seti/kompyutery_i_komplektuyuschie/ssd/"
-
-
-def get_html(url):
-    response = requests.get(url)
-    return response.text
+def get_html(url, headers):
+    session = requests.Session()
+    request = session.get(url, headers=headers)
+    if request.status_code == 200:
+        print("Status code = 200")
+    else:
+        print("NO CONNECTION")
+    return request.text
 
 def write_csv(data):
     with open("data.csv", "a") as file:
         writer = csv.writer(file)
-
-        writer.writerow((data["title"],data["price"],data["link"]))
+        str = f"{data['price']} BYN / {data['units']} {data['curr_abbr']}"
+        writer.writerow([str])
 
 def get_links(html):
     soup = BeautifulSoup(html,"lxml")
-    catalog = soup.find_all("div", class_="catalog-item")
-    print(type(catalog))
-    #print(catalog)
+    trs = soup.find("table", class_="rates-table-nbrb").find("tbody", class_="table-body").find_all("tr")
 
 
-    for item in catalog:
-        link = item.find("div", class_="catalog-item-title").find("a").get("href")
-        title = item.find("div", class_="catalog-item-title").find("a").text
-        price = item.find("div", class_="catalog-item-price").text
-        # print("title = " + str(title))
-        # print("price = " + str(price))
-        # print("link = " + str(link))
-        data = {"link": link, "title": title, "price":price}
+    for tr in trs:
+        price = tr.find("td", class_="course").text
+        units = tr.find("td", class_="code").text
+        curr_abbr = tr.find_all("td")[3].text
 
+        data = {"price":price, "units":units, "curr_abbr":curr_abbr}
 
         write_csv(data)
 
-
-html = get_html(url)
+print("START")
+html = get_html(url, headers)
 get_links(html)
-print("DONE")
+print("Dev01 END")
 
 
